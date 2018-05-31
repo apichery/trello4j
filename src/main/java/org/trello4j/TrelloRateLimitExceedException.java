4ee8ece15499bf086572cafaa220748b729e3812
Arnaud PICHERY <aranud@gmail.com>
2018-05-31 12:24:20 +0200

    Applied changes

---
+++ src/main/java/org/trello4j/TrelloRateLimitExceedException.java
@@ -0,0 +1,14 @@
+package org.trello4j;
+
+public class TrelloRateLimitExceedException extends TrelloException {
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 7427993543996731840L;
+
+    /**
+     * Instantiates a new trello exception.
+     */
+    public TrelloRateLimitExceedException() {
+        super();
+    }
+}
